5ab401507c952ab628ab3af1f78e9ff87e180096
weirshi <woshiweir@yahoo.com>
2020-11-27 11:32:06 +0800

    project init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.less';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      React
+    </div>
+  );
+}
+
+export default App;
---
+++ src/views/login/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login: React.FC = () => {
+    return (
+        <div className="login">
+            Login
+        </div>
+    )
+}
+
+export default Login;
